597438ee48979d1808d8b7ca730d0cb2b4d9f707
AlanMay95 <kontonakumu@gmail.com>
2021-05-27 15:47:55 -0700

    added naming

--- Pypet.py
+++ Pypet.py
@@ -5,6 +5,9 @@ Stage2 = ["Tsunomon","Koromon"]
 Stage3 = ["Gabumon", "Biyomon", "Agumon"]
 
 
+name = ""
+if name == "":
+    name = type
 photo = "('.')"
 type = "Botamon"
 age = 0
@@ -18,10 +21,11 @@ def startup_pypet():
     print("Welcome to Pypet!")
 
 def pypet_stats():
-    print("This is your " + type + " " + photo)
-    print(type + " weighs " + str(weight) + " pounds")
-    print(type + " has " + str(power) + " power")
-    print(type + " has " + str(speed) + " speed")
+    print("This is " + name + " " + photo)
+    print(name + " weighs " + str(weight) + " pounds")
+    print(name + " has " + str(power) + " power")
+    print(name + " has " + str(speed) + " speed")
+    print(name + " is a " + type)
 
     if food < 3:
         print(type + " is hungry!")
@@ -58,6 +62,8 @@ def train():
 #Startup
 startup_pypet()
 
+
+
 terminate = False
 #whileloop
 while not terminate:
@@ -69,7 +75,18 @@ while not terminate:
     if user_input == "quit":
         terminate = True
         print("Goodbye")
+    
+    elif user_input == "commands":
+        print("'Quit' to close")
+        print("'Name' to rename your pet")
+        print("'Stats' to see your pet's stats")
+        print("'Feed' to feed your pet")
+        print("'Train' to improve your pet's stats")
 
+    elif user_input == "name":
+        user_input = input("Enter your pet's new name > ")
+        user_input = name
+            
     elif user_input == "stats":
         pypet_stats()
 
@@ -90,7 +107,7 @@ while not terminate:
         train()
 
     else:
-        print("Sorry there was an error")
+        print("Sorry there was an error. Type 'commands' to see a list of valid commands")
 
 
 
@@ -98,7 +115,7 @@ while not terminate:
     # need to fix type evolved to type - this shows koromon evolved to koromon
     if type == "Botamon" and power > 4 and food > 2:
         type = random.choice(Stage2)
-        print (type + " has evolved to " + type)
+        print ( + " has evolved to " + type)
 
     if type == "Koromon" and power > 9 and food > 2:
         type = "Agumon"
